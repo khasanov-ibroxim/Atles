1d32f1ff72336484216a8c5027f31affd23de331
khasanov_ibroxim <xibroxim11@gmail.com>
2025-01-18 03:02:15 +0500

    ..

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 import logo from '../../Assisstest/img/logo.png'
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import NorthEastIcon from '@mui/icons-material/NorthEast';
 import {Link, useNavigate} from 'react-router-dom';
-export default function Navbar() {
+import {Dropdown, Space} from "antd";
+import {languages} from "../../lang/langs.jsx";
+import {useTranslation} from "react-i18next";
+import {useLanguage} from "../../lang/LangContext.jsx";
+
+export default function Navbar({onlyIcon = true}) {
     const [show, setShow] = useState(false)
     const navigate = useNavigate();
+    const {handleLanguageChange, selectedLanguage} = useLanguage();
+    const {t} = useTranslation();
+
     return (
         <div className="nav-title">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <Link to='/'><img src={logo} alt="" /></Link>
+                        <Link to='/'><img src={logo} alt=""/></Link>
                         <div className={show ? 'null' : 'box'}>
                             <ul>
                                 <li>
@@ -24,21 +32,46 @@ export default function Navbar() {
                                 <li className="dropdown_box">
                                     <div className="dropdown_btn">Производство</div>
                                     <div className="dropdown_menu_box">
-                                        <Link to={"/:id/fabric".replace(":id" , 2)} className="dropdown_menu_item">
+                                        <Link to={"/:id/fabric".replace(":id", 2)} className="dropdown_menu_item">
                                             <>Раскройный цех</>
                                         </Link>
-                                        <Link to={"/:id/fabric".replace(":id" , 1)} className="dropdown_menu_item">
+                                        <Link to={"/:id/fabric".replace(":id", 1)} className="dropdown_menu_item">
                                             <>Швейный цех</>
                                         </Link>
-                                        <Link to={"/:id/fabric".replace(":id" , 4)} className="dropdown_menu_item">
+                                        <Link to={"/:id/fabric".replace(":id", 4)} className="dropdown_menu_item">
                                             <>Контроль качества</>
                                         </Link>
-                                        <Link to={"/:id/fabric".replace(":id" , 3)} className="dropdown_menu_item">
+                                        <Link to={"/:id/fabric".replace(":id", 3)} className="dropdown_menu_item">
                                             <>Гладильно упаковочный цех</>
                                         </Link>
                                     </div>
                                 </li>
 
+                                <li className={"nav_dropdown"}>
+                                    <Dropdown
+                                        menu={{
+                                            items: languages,
+                                            onClick: handleLanguageChange,
+                                        }}
+                                        style={{zIndex: 9999, cursor: "pointer"}}
+                                    >
+                                        <a onClick={(e) => e.preventDefault()}>
+                                            <Space>
+                                                {onlyIcon ? (
+                                                    <span style={{
+                                                        cursor: "pointer",
+                                                        display: "flex",
+                                                        alignItems: "center"
+                                                    }}>{selectedLanguage.icon} <p>{selectedLanguage.label} </p></span>
+                                                ) : (
+                                                    <>
+                                                        {selectedLanguage.icon} {selectedLanguage.label} <DownOutlined/>
+                                                    </>
+                                                )}
+                                            </Space>
+                                        </a>
+                                    </Dropdown>
+                                </li>
 
                                 <li>
                                     <Link className='a-text' to='/contact'>Контакты</Link>
